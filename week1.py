72d0aab2cf691020987c2451bb3f9528921963e5
bonaventura-p <bonaventurapacileo@gmail.com>
2015-06-22 14:23:17 +0200

    submit quiz 1

---
+++ week1.py
@@ -0,0 +1,22 @@
+salary= 10000
+
+if salary >= 1000:
+    donation= float(0.1) * salary
+    print "You can donate" + " " + str(donation) + " " + "this year"
+else:
+   print "No donation this year"
+
+
+   ########
+
+true_username= "karl"
+true_psw= 6666
+
+typed_username= "karl"
+typed_psw= 6666
+
+if typed_username == true_username and typed_psw== 6666:
+    print "Authentication confirmed"
+elif typed_username == true_username and typed_psw == 
+
+
